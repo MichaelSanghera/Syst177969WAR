2780b39f59551ad8900e5d5d9079fe6c5d30a4ee
MichaelSanghera <sanghemi@sheridancollege.ca>
2022-04-11 15:54:47 -0400

    Created Enums

---
+++ src/ca/sheridancollege/project/Value.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Enum.java to edit this template
+ */
+package ca.sheridancollege.project;
+
+/**
+ *
+ * @author HP
+ */
+public enum Value {
+    ACE,
+    TWO,
+    THREE,
+    FOUR,
+    FIVE,
+    SIX,
+    SEVEN,
+    EIGHT,
+    NINE,
+    TEN,
+    JACK,
+    QUEEN,
+    KING;
+    
+}
